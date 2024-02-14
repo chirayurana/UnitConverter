289ff0cd6353be1e320f06af9dd68a075e928b0a
Blinky <chirayurana@yahoo.com>
2024-02-14 18:37:06 +0545

    Added Text style and changed font family

--- app/src/main/java/com/blinky/unitconverter/MainActivity.kt
+++ app/src/main/java/com/blinky/unitconverter/MainActivity.kt
@@ -28,9 +28,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.blinky.unitconverter.ui.theme.UnitConverterTheme
+import java.time.format.TextStyle
 import kotlin.math.roundToInt
 
 class MainActivity : ComponentActivity() {
@@ -49,6 +53,14 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+//Changing text styles and font-family
+val customTextStyle = androidx.compose.ui.text.TextStyle(
+    fontFamily = FontFamily.Monospace,
+    fontSize = 30.sp,
+    color = Color.Red
+)
+
+
 //Unit Converter function 
 @Composable
 fun UnitConverter(){
@@ -75,7 +87,7 @@ fun UnitConverter(){
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "Unit Converter")
+        Text(text = "Unit Converter", style = customTextStyle)
         Spacer(modifier = Modifier.height(16.dp))
         OutlinedTextField(
             value = inputValue,
@@ -170,7 +182,8 @@ fun UnitConverter(){
             
         }
         Spacer(modifier = Modifier.height(16.dp))
-        Text(text = "Result: $outputValue $outputUnit")
+        Text(text = "Result: $outputValue $outputUnit",
+            style = MaterialTheme.typography.headlineMedium)
 
     }
 
